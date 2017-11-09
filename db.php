edb6603f4c9022c3ccc7ab670f293181ddba5722
grayowldev <truegrayowl@gmail.com>
2017-11-09 09:41:28 -0500

    final

--- db.php
+++ db.php
@@ -5,18 +5,13 @@
  * Date: 11/5/17
  * Time: 4:43 PM
  */
-
 $sqlServerName = "sql2.njit.edu";
 $sqlUserName = "";
 $sqlPass = "";
 $dbName = "";
-
 $username = $_POST['username'];
 $password = $_POST['password'];
-
-
 $connection = mysqli_connect($sqlServerName, $sqlUserName, $sqlPass, $dbName);
-
 $sql = "SELECT * FROM users WHERE username='$username'";
 $results = mysqli_query($connection, $sql);
 $resultCheck = mysqli_num_rows($results);
@@ -37,19 +32,41 @@ if ($resultCheck = 1){
         }
     }
 }
-
-function lookupGrade($type){
+function lookupGrade($type,$class,$conn,$userID){
     if ($type == 'Student' || $type == 'Faculty'){
-
+        if($type == Faculty){
+            $sql = "SELECT * FROM grades_lookup";
+            $json = array([]);
+            $i = 0;
+            $result = mysqli_fetch_array(conn, $sql);
+            foreach ($result as &$value){
+                $json[$i] = $value;
+                $i += 1;
+            }
+            echo $json;
+        }
+    } else if (type == 'Student'){
+        $sql = "SELECT * FROM grades_lookup WHERE studentid = $userID";
+        $result = mysqli_query_row($conn, $sql);
+        $json = array(mysqli_fetch_row($result));
+        echo $json;
     }
 }
-
-function addGrades($type, $username){
-
+function addGrades($type, $username,$grade){
+    if ($type == 'Faculty'){
+        $update = "UPDATE grades_lookup SET grade='$grade' WHERE studentid=$username";
+        echo "ok";
+    }
 }
-
-function editGrades($type, $username){}
-
-function courseLookup(){
-
+function editGrades($type, $username,$grade){
+    if ($type == 'Exercutive'){
+        $update = "UPDATE grades_lookup SET grade='$grade' WHERE studentid=$username";
+        echo "ok";
+    }
+}
+function courseLookup($conn,$userID){
+    $sql = "SELECT * FROM course_lookup WHERE studentid = $userID";
+    $result = mysqli_query_row($conn, $sql);
+    $json = array(mysqli_fetch_row($result));
+    echo $json;
 }
